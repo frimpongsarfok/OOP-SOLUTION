3c114b24f9ce516bf829b8027c028237fec78259
Sarfo Kantanka Frimpong <sarfokantankafrimpong@gmail.com>
2022-01-21 15:45:37 -0500

    room solution

--- src/room.js
+++ src/room.js
@@ -1,5 +1,18 @@
 class Room {
-  
+  constructor(...param){
+    this.name=param[0];
+    this.description=param[1];
+    this.contents=[];
+   
+  }
+ 
+  add(item){
+    this.contents.push(item);
+    return this;
+  }
+  has(item){
+      return this.contents.includes(item);
+  }
 }
 
 module.exports = Room
