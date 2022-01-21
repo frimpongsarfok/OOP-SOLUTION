ca8c250f7c85f14d8dfcc8b325563b542045871d
Sarfo Kantanka Frimpong <sarfokantankafrimpong@gmail.com>
2022-01-21 15:45:18 -0500

    marker solution

--- src/marker.js
+++ src/marker.js
@@ -1,5 +1,25 @@
 class Marker {
-  
+   constructor(...param){
+       this.size=param[0];
+       this.color=param[1];
+       this.remainingInk=param[2];
+   }
+   write(string){
+        let ink="";
+       for(let char of string){
+         if(char!=' ' && this.remainingInk){
+            this.remainingInk--;
+            ink+=char;
+         }else if(char===' '){
+             ink+=char;
+         }
+         
+       }
+       if(!this.remainingInk){
+            return ink;
+       }
+      return string; 
+   }
 }
 
 module.exports = Marker
