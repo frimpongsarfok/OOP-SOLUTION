2b258d3eb5dde0e5a9557d623b0ebc7e703ac79f
Sarfo Kantanka Frimpong <sarfokantankafrimpong@gmail.com>
2022-01-21 15:44:56 -0500

    desk solution

--- src/desk.js
+++ src/desk.js
@@ -1,5 +1,20 @@
 class Desk {
-  
+  constructor(...param){
+      this.length=param[0];
+      this.width=param[1];
+      this.height=param[2];
+      this.isWhiteboard=param[3];
+      this.content="";
+     
+  }
+  write(strPram){
+      this.content+=strPram;
+  }
+  wipe(){
+      if(this.isWhiteboard){
+          this.content="";
+      }
+  }
 }
 
 module.exports = Desk
